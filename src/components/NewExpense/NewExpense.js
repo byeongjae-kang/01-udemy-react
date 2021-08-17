38fc8e2e4e3f1f768eda8abe90505ffb71379535
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-17 00:11:22 -0700

    add conditional state show and hide expense form accordingly

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseData = (enteredExpenseData) => {
     const expenseData = {
       id: Math.random().toString(),
       ...enteredExpenseData
     };
     props.onAddExpense(expenseData);
-    
+    setIsEditing(false);
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditinghandler = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseData} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseData}
+          onCancel={stopEditinghandler}
+        />
+      )}
     </div>
   );
 };
